6b943cff2f20a37f4fc9a396ac34f0285fdf5aaf
canhakan <canhakandagidir@gmail.com>
2021-12-09 13:55:28 +0300

    add_to_tables() now changes global variables
    I probably will delete these functions from the package. But just for playing with global variables I tried that.

--- R/add_to_tables.R
+++ R/add_to_tables.R
@@ -5,18 +5,15 @@
 #' @description Adds models results to two tables. First one is big table, where every observation and prediction is visible. Second one is small table where only model name, performance and notes are visible.
 #' @param data is the full data with 'y' values and all
 #' @param predictions is the model's prediction results
-#' @param big_table is the existing table of all results
-#' @param small_table is the existing table of summarised results
 #' @param model_name is the name of the model that produced the predictions. For note taking purposes, only.
 #' @param notes is additional notes for small_table
 #' @return a list of tables. first element is the 'big_table', second one is the 'small_table'.
 #'     Big table contains: date, year, month, hour, model name, actual results, predicted results and WMAPE
 #'     Small table contains: model name, WMAPE and notes
 #' @export
+#'
 add_to_tables <- function(data,
                           predictions,
-                          big_table,
-                          small_table,
                           model_name,
                           notes='-'){
   # getting year and month
@@ -36,11 +33,10 @@ add_to_tables <- function(data,
                    wmape = err)
   # error at predicted and wmape : colnames are predicted.1 and wmape.1 (prolly because of others named that way)
   colnames(res)[7:8] <- c('predicted','wmape')
-  big_table = rbind(big_table,res)
-
+  pkg.env$big_table = rbind(pkg.env$big_table,res)
   res2 = data.table::data.table(model = model_name,
                    wmape = err,
                    notes = notes)
-  small_table = rbind(small_table,res2)
-  return(list(big_table,small_table))
+  pkg.env$small_table = rbind(pkg.env$small_table,res2)
+  return(pkg.env$big_table, pkg.env$small_table)
 }
